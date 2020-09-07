44b715086542650c3caaa2b6d1266af3be2cb5b3
jenniekibiri <josphatje@gmail.com>
2020-09-07 16:21:01 +0300

    fetch api from open weather"

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//variables
+let temperature = document.getElementById('temperature')
+let rainDesc = document.getElementById('rainDescription')
+const icon = document.querySelector(".weather-icon")
+//call the apis and remember to add your api keys
+const api = `http://api.openweathermap.org/data/2.5/weather?q=Nyeri&appid=2a8705842347be34e28960ed24c50698`
+fetch(api)
+           .then(response=>{
+               //the console will tell you the status of call
+               console.log(response)
+               return response.json()
+               
+           }).then(data=>{
+               //the data returned by the call you made 
+               console.log(data)
+               const temp=data.main.temp
+               //convert the kelvin to celcius
+               let celcius = Math.round((temp-273.15)*10)/10
+                temperature.innerHTML= ` <h4 class="text-white">${celcius}&#176;</h4>`
+               
+
+
+               rainDesc.textContent=data.weather[0].description
+                icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>`;
+           })
+          
